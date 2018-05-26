b4469031cc432cbb009479c391c7156adadb706c
jfuertes <jfuertesl2@gmail.com>
2018-05-26 15:26:31 -0500

    php

--- sistema/api/config/config.php
+++ sistema/api/config/config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'esdeporv_vacuna');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'alarmas');
+define('DB_NAME', 'bcp_go');
 
 
 
