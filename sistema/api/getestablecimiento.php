f49cb2c55fc154127aba5331bbed7a4cc9cb060b
jfuertes <jfuertesl2@gmail.com>
2018-05-26 16:02:56 -0500

    php python

--- sistema/api/getestablecimiento.php
+++ sistema/api/getestablecimiento.php
@@ -5,11 +5,16 @@
 		$db              = new Connect();
 		$dbh             = $db->enchufalo();
 
+ $id=$_POST["id"];
+ $tiempo_estimado=$_POST["tiempo_estimado"];
 
-		$q = 'SELECT  *
-				from establecimientos';
+ $q = 'UPDATE establecimientos SET tiempo_estimado= :tiempo_estimado where id=:id';
+
+		
 
 		$stmt = $dbh->prepare($q);
+		$stmt->bindParam(':id',  $id, PDO::PARAM_STR);
+		$stmt->bindParam(':tiempo_estimado',  $tiempo_estimado, PDO::PARAM_STR);
 		$stmt->execute();
 		$r = $stmt->fetchAll(PDO::FETCH_ASSOC);
 		echo json_encode($r);
